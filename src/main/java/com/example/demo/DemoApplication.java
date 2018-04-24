9316e5b1065bc7b22286e9e186f1cdfd2317a4c6
boolbit <582076225@qq.com>
2018-04-24 09:46:39 +0800

    print feature

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,6 +9,7 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		System.out.println("master");
 		System.out.println("dev");
+		System.out.println("feature");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 }
